9bc26aa26077556ccbcbd4a7cc35ac5b88b69824
John Janecek <janecektyler@gmail.com>
2017-06-01 17:29:28 -0700

    renamed chessAI to AI.py

--- PlayMatches.py
+++ PlayMatches.py
@@ -1,24 +1,32 @@
 from chess import uci
 from chess import Board
-import engines.ChessAI as ChessAI
+import engines.AI as AI
 import copy
 
-engine = uci.popen_engine('engines/stockfish')
-engine.uci()
+def runMatches(engine):
 
-myAI = ChessAI.ChessAI()
-board = Board()
+	engine = uci.popen_engine('engines/stockfish')
+	engine.uci()
 
-while not board.is_stalemate() and not board.is_game_over():
-	m = myAI.getMove(copy.deepcopy(board))
-	board.push_uci(m)	
+	myAI = AI.NN_AI()
 
-	if board.is_stalemate() or board.is_game_over():
-		break; 
-	
-	engine.position(board)
-	engine_move = str(engine.go(movetime=1)[0])
-	board.push_uci(engine_move)
 
-print(board)
-	
+	for i in range(1,10):
+		board = Board()
+		while not board.is_stalemate() and not board.is_game_over():
+			m = myAI.getMove(copy.deepcopy(board))
+			board.push_uci(m)	
+
+			if board.is_stalemate() or board.is_game_over():
+				break; 
+			
+			engine.position(copy.deepcopy(board))
+			engine_move = str(engine.go(movetime=5, depth=i)[0])
+			board.push_uci(engine_move)
+
+		print('Depth:',i, 'Result:',board.result())
+
+
+runMatches('engines/stockfish')
+
+		
